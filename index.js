b3f263f8ffd4b10e33030ed63c1594b6dd6853ab
Javeriya <121876323+jennylewis45@users.noreply.github.com>
2023-05-27 01:34:12 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+// const cursor=document.querySelector(".cursor")
+// var timeout;
+
+// //cursor movement
+// document.addEventListener("mousemove",(e)=>{
+//     let x=e.pageX
+//     let y=e.pageY
+
+//     cursor.style.top=y+ 'px'
+//     cursor.style.left=x+ 'px'
+//     cursor.style.display='block'
+
+//     //stop animation
+
+
+//     function mouseStopped(){
+//         cursor.style.display='none'
+//     }
+
+// //hide animation
+// clearTimeout(timeout)
+// timeout=setTimeout(mouseStopped,1000) 
+
+// })
+
+// //stop animation of the screen
+
+// document.addEventListener("mouseout",()=>{
+//     cursor.style.display='none'
+// }
+// )
+
+
+
+const cursor=document.querySelector(".cursor")
+var timeout;
+
+document.addEventListener("mousemove",(e)=>{
+    let x=e.pageX
+    let y=e.pageY
+
+    cursor.style.top=y+"px"
+    cursor.style.left=x+"px"
+    cursor.style.display='block'
+
+    function mouseStopped(){
+        cursor.style.display='none'
+
+    }
+
+    clearTimeout(timeout)
+    timeout=setTimeout(mouseStopped,1000)
+        
+   
+
+
+})
+
+document.addEventListener("mouseout",()=>{
+    cursor.style.display='none'
+
+}
+)
